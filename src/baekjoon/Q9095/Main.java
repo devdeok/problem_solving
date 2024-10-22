78e95dfb79390975fb8c5ee2d79172c272ab560d
jaedeok <dlwoejr700@gmail.com>
2024-10-23 01:26:46 +0900

    feat : 9095

--- src/baekjoon/Q9095/Main.java
+++ src/baekjoon/Q9095/Main.java
@@ -1,20 +1,31 @@
 package baekjoon.Q9095;
 
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         // 엔터로 구분해서 입력 받음
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        // 백준 문제는 입력을 띄어쓰기로 구분하는 경우가 있기 때문에 StringTokenizer 사용
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int result = 0;
+        int T = Integer.parseInt(br.readLine()); // 테스트 케이스 개수
+        int[] dp = new int[12]; // dp를 사용할 배열
 
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
 
-        System.out.println(result);
+        // 4 7 10
+        for(int i = 4 ; i <= 11; i++){
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+//            System.out.println("index : " + i + " / value :" + dp[i]);
+        }
+
+        for(int j = 0; j<T; j++){
+            int n = Integer.parseInt(br.readLine());
+            System.out.println(dp[n]);
+        }
+
 
         bw.flush();
     }
