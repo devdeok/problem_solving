8209a2a655d9e6ab8f44dd53e13eb88bea381111
JAEDEOK <jaedeok_lee@tmax.co.kr>
2024-10-23 13:19:17 +0900

    feat : 1149

--- src/baekjoon/Q1149/Main.java
+++ src/baekjoon/Q1149/Main.java
@@ -1,24 +1,31 @@
 package baekjoon.Q1149;
 
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         // 엔터로 구분해서 입력 받음
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
         int N = Integer.parseInt(br.readLine());
+        int[][] cost = new int[N][3];
 
-        int[] dp = new int[N+1];
+        for(int i = 0; i < N ; i++){
+            st = new StringTokenizer(br.readLine(), " ");
 
-        for(int i =0; i <= N ; i++){
-            String[] rgb = br.readLine().split(" ");
-
-            for(int j=0; j<rgb.length;j++)
-                System.out.println("rgb : " + rgb[j]);
-
+            cost[i][0] = Integer.parseInt(st.nextToken()); // R
+            cost[i][1] = Integer.parseInt(st.nextToken()); // G
+            cost[i][2] = Integer.parseInt(st.nextToken()); // B
         }
+
+        for(int i = 1; i < N ; i++){
+            cost[i][0] = cost[i][0] + Math.min(cost[i-1][1], cost[i-1][2]);
+            cost[i][1] = cost[i][1] + Math.min(cost[i-1][0], cost[i-1][2]);
+            cost[i][2] = cost[i][2] + Math.min(cost[i-1][0], cost[i-1][1]);
+        }
+
+        System.out.println(Math.min(cost[N-1][0], Math.min(cost[N-1][1], cost[N-1][2])));
     }
 }
